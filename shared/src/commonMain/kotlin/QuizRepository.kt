56b205ec0a3658b0f5c0eb2a28a1180b4d5d289a
Yann-vdv <61906860+Yann-vdv@users.noreply.github.com>
2023-12-15 15:24:14 +0100

    handle no connection with mock data

--- shared/src/commonMain/kotlin/QuizRepository.kt
+++ shared/src/commonMain/kotlin/QuizRepository.kt
@@ -5,10 +5,12 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import network.CacheQuizAPI
 import network.QuizAPI
 
 class QuizRepository {
     private val quizAPI = QuizAPI()
+    private val cacheQuizAPI = CacheQuizAPI()
     private val coroutineScope = CoroutineScope(Dispatchers.Default)
 
     private var _questionState=  MutableStateFlow(listOf<Question>())
@@ -20,33 +22,16 @@ class QuizRepository {
 
     private suspend fun fetchQuiz(): List<Question> = quizAPI.getAllQuestions().questions
 
+    private suspend fun fetchCacheQuiz(): List<Question> = cacheQuizAPI.getAllQuestions().questions
+
     private fun updateQuiz(){
 
         coroutineScope.launch {
-            val questions = fetchQuiz();
-            if (questions.size > 0) {
-                _questionState.update { questions }
-            } else {
-                _questionState.update { listOf(
-                    Question(id = 0, correctAnswerId = 0, label = "Android is a great platform ?", answers = listOf(
-                        Answer(id = 0, label = "Yes"),
-                        Answer(id = 1, label = "No")
-                    )),
-                    Question(id = 1, correctAnswerId = 1, label = "Android = IOS ?", answers = listOf(
-                        Answer(id = 0, label = "Yes"),
-                        Answer(id = 1, label = "No")
-                    )),
-                    Question(id = 2, correctAnswerId = 0, label = "Quel est ce cours ?", answers = listOf(
-                        Answer(id = 0, label = "Mobile"),
-                        Answer(id = 1, label = "Management")
-                    )),
-                    Question(id = 3, correctAnswerId = 2, label = "quelle est la couleur du cheval blanc d'Henry IV ?", answers = listOf(
-                        Answer(id = 0, label = "Noir"),
-                        Answer(id = 1, label = "Rouge"),
-                        Answer(id = 2, label = "Blanc"),
-                        Answer(id = 3, label = "Arc en ciel")
-                    ))
-                ) }
+            try {
+                _questionState.update { fetchQuiz() }
+            } catch (err:Exception) {
+                //println(err)
+                _questionState.update { fetchCacheQuiz() }
             }
         }
     }
