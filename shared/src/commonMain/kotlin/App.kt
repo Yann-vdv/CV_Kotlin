20f709e95865eb54bfdab761485dfe369f986a15
Yann-vdv <61906860+Yann-vdv@users.noreply.github.com>
2024-01-11 21:38:31 +0100

    start stawars quizz

--- shared/src/commonMain/kotlin/App.kt
+++ shared/src/commonMain/kotlin/App.kt
@@ -32,8 +32,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import data.CvData
-import data.ParData
 import data.Question
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
@@ -42,6 +40,7 @@ import moe.tlaster.precompose.navigation.NavHost
 import moe.tlaster.precompose.navigation.Navigator
 import moe.tlaster.precompose.navigation.path
 import moe.tlaster.precompose.navigation.rememberNavigator
+import network.QuizRepository
 
 private val repository = QuizRepository()
 
@@ -71,86 +70,6 @@ fun App() {
                 }
             }
         }
-    }
-//        CV(CvData());
-}
-
-@Composable
-@OptIn(ExperimentalResourceApi::class)
-internal fun CV(data: CvData) {
-    Row(modifier = Modifier.fillMaxHeight().verticalScroll(rememberScrollState())) {
-        Spacer(modifier = Modifier.padding(start = 10.dp))
-        Column(modifier = Modifier.width(250.dp)) {
-//                        AsyncImage(
-//                            model = "https://cdn-icons-png.flaticon.com/512/3135/3135715.png",
-//                            contentDescription = "Photo de profil"
-//                        )
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                modifier = Modifier.width(250.dp)
-            ) {
-                Image(
-                    painterResource("compose-multiplatform.xml"),
-                    contentDescription = "Compose Multiplatform icon",
-                    modifier = Modifier.size(140.dp)
-                )
-                Text(
-                    data.lastName,
-                    fontWeight = FontWeight.Bold,
-                    fontSize = 25.sp,
-                    textAlign = TextAlign.Center
-                )
-                Text(data.firstName, fontWeight = FontWeight.Bold, fontSize = 25.sp)
-                Text(data.age.toString() + " ans", fontWeight = FontWeight.SemiBold)
-                Text(data.subject, fontWeight = FontWeight.SemiBold)
-            }
-            Text("PROFIL", fontWeight = FontWeight.Bold, fontSize = 18.sp)
-            Text(data.profilDesc)
-            Text("LANGUES", fontWeight = FontWeight.Bold, fontSize = 18.sp)
-            data.languages.forEach { langue ->
-                Language(
-                    title = langue.name,
-                    score = langue.score
-                )
-            }
-            Text("CONTACT", fontWeight = FontWeight.Bold, fontSize = 18.sp)
-            Text("TELEPHONE :", fontWeight = FontWeight.Bold, fontSize = 12.sp)
-            Text(data.phone)
-            Text("E-MAIL :", fontWeight = FontWeight.Bold, fontSize = 12.sp)
-            Text(data.email)
-        }
-        Spacer(modifier = Modifier.padding(start = 16.dp))
-        Column(modifier = Modifier.padding(end = 20.dp)) {
-            Text(
-                "EXPERIENCES PROFESSIONNELLES",
-                fontWeight = FontWeight.Bold,
-                fontSize = 25.sp
-            )
-            data.paragraph.forEach { para -> Par(para) }
-            Text("FORMATIONS", fontWeight = FontWeight.Bold, fontSize = 25.sp)
-            data.paragraph2.forEach { para -> Par(para) }
-        }
-    }
-}
-@Composable
-internal fun Par(data: ParData) {
-    Spacer(modifier = Modifier.padding(top = 10.dp))
-    Column {
-        Text(text = data.title, fontWeight = FontWeight.Bold)
-        Text(data.date)
-        Text(data.description)
-    }
-}
-@Composable
-internal fun Language(title: String, score: Int) {
-    Row {
-        Text(text = title, fontWeight = FontWeight.SemiBold)
-        Spacer(modifier = Modifier.padding(end = 5.dp))
-        if (score == 1) Text(text = "X")
-        else if (score==2) Text(text = "XX")
-        else if (score==3) Text(text = "XXX")
-        else if (score==4) Text(text = "XXXX")
-        else if (score==5) Text(text = "XXXXX")
     }
 }
 
